59ac81acd78ff3606e2d9e3db6338ab9d47a5d91
Olga <olyushkam@gmail.com>
2019-05-19 17:08:00 +0300

    THIS DOES NOT WORK.
    + defined aliases for int and float types
    + formed Input_Data and DVector structures
    + added Interpolate function, which calculate regression coefs (a,b in docs)
    - got a problem with a pointer. you'll see under the fixme comment

--- headers/interpolation.h
+++ headers/interpolation.h
@@ -5,21 +5,35 @@
 #ifndef INTERPOLATION_MOSKANOVA_INTERPOLATION_H
 #define INTERPOLATION_MOSKANOVA_INTERPOLATION_H
 
-#include <lzma.h>
+#define integer int
+#define floating_point float
+
+#include <lzma.h> // get size_t thing
+
+//================ STRUCTURES DEFINITION SECTION  ==============
 
 
 struct DVector{
     size_t dim;
+    floating_point *coordinates;
 };
 typedef struct DVector DVector;
 
 
 struct Input_Data{
-    struct DVector x, y;
+    int n; // length of arrays.
+    struct DVector *x, *y;
 };
 typedef struct Input_Data Input_Data;
 
+//================ TASK FUNCTIONS SECTION ==============
 
-struct DVector* Interpolate(struct Input_Data* input);
+struct DVector Interpolate(const struct Input_Data* input);
+
+//================ INPUT SECTION ==============
+//void input_vector();
+//void finput_vector();
+
+//================ OUTPUT SECTION ==============
 
 #endif //INTERPOLATION_MOSKANOVA_INTERPOLATION_H
--- interpolation.c
+++ interpolation.c
@@ -2,35 +2,43 @@
 // Created by olga on 23.04.19.
 //
 
-#include <lzma.h>
-
-struct DVector{
-    size_t dim;
-    // float?? coordinates
-};
-
-struct Input_Data{
-    /*
-     * x - height
-     * y - weight
-     *
-     * */
-    struct DVector x, y;
-};
+#include <malloc.h>
+#include "headers/interpolation.h"
 
 // returns a and b ???
-struct DVector* Interpolate(struct Input_Data* input){
-    struct DVector* res = {0};
+struct DVector Interpolate(const struct Input_Data* input){
+    // define result vector, which contains 2 coordinates
+    // res.coordinates[0] - a
+    // res.coordinates[1] - b
+    // y = ax + b
+    if (input->n <=0) {
+        // fixme: do something like program exiting
+        printf("Your dataset is empty, just like your head, dude.");
+    }
+
+    struct DVector res = {2};
+    res.coordinates = (floating_point *) calloc(2, sizeof(floating_point));
+
+    // a
+    floating_point a_numerator = 0;
+    floating_point a_denominator = 0;
+    floating_point buff;
+    integer nn = input->n;
+    for (integer i = 0; i < nn; ++i) {
+        buff = nn * input->x->coordinates[i] - 1;
+        a_numerator   += input->y->coordinates[i] * buff;
+        a_denominator += input->x->coordinates[i] * buff;
+    }
+    res.coordinates[0] = a_numerator / a_denominator;
+
+    // b
+    floating_point b_numerator = 0;
+    for (integer i = 0; i < nn; ++i) {
+        b_numerator += input->y->coordinates[i];
+        b_numerator -= input->x->coordinates[i] *
+                       res.coordinates[0];
+    }
+    res.coordinates[1] = b_numerator / nn;
 
     return res;
 }
-
-// input from stdin
-void input_vector(){
-
-}
-
-// input from text file
-void finput_vector(){
-
-};
